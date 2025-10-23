00e9c54dd23531f670cbc962a8b5010565c2736a
last.lantican <lastdavid.lantican@gmail.com>
2025-10-24 06:10:31 +0800

    done: export side, wip: import side

--- popup.js
+++ popup.js
@@ -3,28 +3,63 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const plugin = {
     websites: ["buildertrend.net", "appfolio.com"],
+    work_orders: new Map(),
 
     start: async function () {
-      // wip
-
       // check if on Appfolio or Buildertrend
-      const currentTab = await this.getActiveTabURL();
-      if (!this.websites.some((domain) => currentTab.url.includes(domain))) {
-        this.criticalError(
-          "You must be on Appfolio or Buildertrend to use this plugin."
-        );
-        this.load(false);
-        return;
-      }
+      // const currentTab = await this.getActiveTabURL();
+      // if (!this.websites.some((domain) => currentTab.url.includes(domain))) {
+      //   this.criticalError(
+      //     "You must be on Appfolio or Buildertrend to use this plugin."
+      //   );
+      //   this.load(false);
+      //   return;
+      // }
+
+      const clearButton = document.querySelector("footer button.clear");
+      if (!clearButton) throw new Error("Clear button not found.");
 
       // propagate work orders from the storage
+      chrome.storage.local.get("work_orders", (result) => {
+        const data = result.work_orders || {};
+        if (typeof data !== "object") throw new Error("Invalid map data.");
 
-      // if work orders are not empty, show clear all button
+        this.work_orders = new Map(Object.entries(data));
 
-      this.load(false);
+        // show the list items
+        const workOrdersList = document.querySelector("#work-orders");
+        if (!workOrdersList) throw new Error("Work Order list not found.");
+
+        if (this.work_orders.size > 0) {
+          this.work_orders.forEach((work_order) => {
+            this.showWorkOrder(workOrdersList, work_order);
+          });
+
+          // if work orders are not empty, show clear all button
+          clearButton.addEventListener("click", () => {
+            // BUG: multiple confirmation when deleting 1 by 1 THEN clearing all at once
+            // confirm("Clearing all work orders cannot be undone. Continue?");
+
+            chrome.storage.local.clear();
+            clearButton.setAttribute("data-hidden", true);
+          });
+
+          clearButton.setAttribute("data-hidden", false);
+        } else {
+          const p = document.createElement("p");
+          p.textContent = "You currently have no work orders saved.";
+          workOrdersList.appendChild(p);
+
+          clearButton.setAttribute("data-hidden", true);
+        }
+
+        this.load(false);
+      });
     },
 
     load: function (bool) {
+      if (typeof bool !== "boolean") throw new Error("Invalid bool.");
+
       setTimeout(() => {
         document.body.setAttribute("data-loading", bool);
       }, 0);
@@ -39,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (option.timeout !== undefined && typeof option.timeout !== "number")
         throw new Error("Invalid timeout.");
 
-      const flash = this.queryElement("#flash");
+      const flash = document.querySelector("#flash");
       if (!flash) throw new Error("Flash is missing.");
 
       const li = document.createElement("li");
@@ -64,7 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (option.timeout !== undefined && typeof option.timeout !== "number")
         throw new Error("Invalid timeout.");
 
-      const flash = this.queryElement("#flash");
+      const flash = document.querySelector("#flash");
       if (!flash) throw new Error("Flash is missing.");
 
       const li = document.createElement("li");
@@ -94,23 +129,6 @@ document.addEventListener("DOMContentLoaded", () => {
       this.terminate();
     },
 
-    queryElement: function (selector, timeout = 10000) {
-      if (typeof selector !== "string") throw new Error("Invalid selector.");
-      if (timeout !== undefined && typeof timeout !== "number")
-        throw new Error("Invalid timeout.");
-
-      let element = document.querySelector(selector);
-      if (element) return element;
-
-      const startTime = Date.now();
-
-      while (!element && Date.now() - startTime < timeout) {
-        element = document.querySelector(selector);
-      }
-
-      return element;
-    },
-
     getActiveTabURL: async function () {
       const [tab] = await browser.tabs.query({
         currentWindow: true,
@@ -120,8 +138,148 @@ document.addEventListener("DOMContentLoaded", () => {
       return tab;
     },
 
-    listen: function (msg, sender, senderResponse) {
-      // wip
+    showWorkOrder: function (listElement, work_order) {
+      if (!listElement instanceof Element)
+        throw new Error("Invalid list element.");
+
+      if (!work_order || typeof work_order !== "object")
+        throw new Error("Invalid work order.");
+
+      const li = document.createElement("li");
+
+      const workOrderNo = document.createElement("div");
+      workOrderNo.className = "work-order-no";
+
+      const pWorkOrderNo = document.createElement("p");
+      pWorkOrderNo.className = "copyable";
+      pWorkOrderNo.textContent = work_order.number || "";
+
+      workOrderNo.appendChild(pWorkOrderNo);
+      li.appendChild(workOrderNo);
+
+      const unit = document.createElement("div");
+      unit.className = "unit";
+
+      const pStreet = document.createElement("p");
+      pStreet.className = "street copyable";
+      pStreet.textContent = work_order.street || "";
+
+      const pCityStateZip = document.createElement("p");
+
+      const spanCity = document.createElement("span");
+      spanCity.className = "city";
+      spanCity.textContent = (work_order.city || "") + ",";
+
+      const spanState = document.createElement("span");
+      spanState.className = "state";
+      spanState.textContent = work_order.state || "";
+
+      const spanZip = document.createElement("span");
+      spanZip.className = "zip";
+      spanZip.textContent = work_order.zip || "";
+
+      pCityStateZip.appendChild(spanCity);
+      pCityStateZip.appendChild(spanState);
+      pCityStateZip.appendChild(spanZip);
+      unit.appendChild(pStreet);
+      unit.appendChild(pCityStateZip);
+      li.appendChild(unit);
+
+      const description = document.createElement("div");
+      description.className = "description";
+      description.title = work_order.description || "";
+
+      const pDescription = document.createElement("p");
+      pDescription.className = "copyable";
+      pDescription.textContent = work_order.description || "";
+
+      description.appendChild(pDescription);
+      li.appendChild(description);
+
+      const actions = document.createElement("div");
+      actions.className = "actions";
+
+      const buttonInsert = document.createElement("button");
+      buttonInsert.setAttribute("type", "button");
+      buttonInsert.className = "insert";
+      buttonInsert.title = "Fill";
+
+      const iconInsert = document.createElement("i");
+      iconInsert.className = "icon";
+
+      const svgInsert = `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+              <path
+                d="M539.3 64.1C549.2 63.3 558.9 67.1 565.9 74.1C572.9 81.1 576.7 90.8 575.9 100.7C571.9 150 558.5 226.9 529.6 300.4C527.8 304.9 524.1 308.3 519.4 309.7L438.5 334C434.6 335.2 432 338.7 432 342.8C432 347.9 436.1 352 441.2 352L479.8 352C491.8 352 499.5 364.8 493.3 375.1C489.3 381.8 485 388.3 480.6 394.7C478.6 397.6 475.6 399.7 472.2 400.8L374.5 430C370.6 431.2 368 434.7 368 438.8C368 443.9 372.1 448 377.2 448L393.2 448C407.8 448 414.2 465.4 402 473.4C334 518.4 264.3 516.7 219.6 504.7C206.9 501.3 195.6 494.8 185.2 486.8L112 560C103.2 568.8 88.8 568.8 80 560C71.2 551.2 71.2 536.8 80 528L160 448L160.5 448.5C161.2 447.2 162.1 446 163.2 444.9L320 288C328.8 279.2 328.8 264.8 320 256C311.2 247.2 296.8 247.2 288 256L153.7 390.2C144.8 399.1 129.7 394.6 128.7 382C124.4 328.8 138 258.9 201.3 195.6C292.4 104.5 455.5 70.9 539.2 64.1z"
+              />
+            </svg>
+          `;
+      iconInsert.innerHTML = svgInsert;
+
+      const spanInsert = document.createElement("span");
+      spanInsert.className = "sr-only";
+      spanInsert.textContent = "Insert";
+
+      // WIP: add fill out click listener
+
+      const buttonDelete = document.createElement("button");
+      buttonDelete.setAttribute("type", "button");
+      buttonDelete.className = "delete";
+      buttonDelete.title = "Remove";
+
+      const iconDelete = document.createElement("i");
+      iconDelete.className = "icon";
+
+      const svgDelete = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+      <path
+      d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"
+      />
+      </svg>
+      `;
+      iconDelete.innerHTML = svgDelete;
+
+      const spanDelete = document.createElement("span");
+      spanDelete.className = "sr-only";
+      spanDelete.textContent = "Delete";
+
+      buttonDelete.addEventListener("click", () => {
+        this.removeWorkOrder(work_order);
+      });
+
+      buttonInsert.appendChild(iconInsert);
+      buttonInsert.appendChild(spanInsert);
+
+      buttonDelete.appendChild(iconDelete);
+      buttonDelete.appendChild(spanDelete);
+
+      actions.appendChild(buttonInsert);
+      actions.appendChild(buttonDelete);
+
+      li.appendChild(actions);
+
+      listElement.appendChild(li);
+    },
+
+    removeWorkOrder: async function (work_order) {
+      if (!work_order || typeof work_order !== "object")
+        throw new Error("Invalid work order.");
+
+      chrome.storage.local.get("work_orders", (result) => {
+        const data = result.work_orders || {};
+        if (typeof data !== "object") throw new Error("Invalid map data.");
+
+        this.work_orders = new Map(Object.entries(data));
+
+        if (!this.work_orders.has(work_order.number || "")) return;
+
+        this.work_orders.delete(work_order.number);
+
+        chrome.storage.local.set(
+          { work_orders: Object.fromEntries(this.work_orders) },
+          () => {}
+        );
+      });
     },
   };
 
@@ -129,8 +287,13 @@ document.addEventListener("DOMContentLoaded", () => {
     plugin.start();
   });
 
-  browser.runtime.onMessage.addListener((msg, sender, senderResponse) => {
-    plugin.listen(msg, sender, senderResponse);
-    return true;
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName !== "local" && !changes.work_orders) return;
+
+    const workOrders = document.querySelectorAll("#work-orders > li");
+    if (!workOrders) return;
+
+    workOrders.forEach((button) => button.remove());
+    plugin.start();
   });
 });
--- work_orders.js
+++ work_orders.js
@@ -1,3 +1,251 @@
 (() => {
-  console.log("test");
+  const content = {
+    save_button_class: "nss-save-work-order",
+    remove_button_class: "nss-remove-work-order",
+    work_orders: new Map(),
+
+    init: async function () {
+      // chrome.storage.local.clear();
+
+      const workOrderList = await this.queryElement(
+        ".js-work-order-list ul.list-group"
+      );
+      if (!workOrderList) return;
+
+      const workOrderItems = workOrderList.querySelectorAll(
+        ":scope > li.list-group-item"
+      );
+      if (!workOrderItems) return;
+
+      chrome.storage.local.get("work_orders", (result) => {
+        const data = result.work_orders || {};
+        if (typeof data !== "object") throw new Error("Invalid map data.");
+
+        this.work_orders = new Map(Object.entries(data));
+
+        workOrderItems.forEach((workOrderItem) => {
+          // check if already exist in work_orders storage
+          const workOrderNumber =
+            workOrderItem.querySelector(
+              ".js-work-order-number [aria-label='Number']"
+            ).textContent || null;
+          if (!workOrderNumber) return;
+
+          if (this.work_orders.has(workOrderNumber)) {
+            this.insertRemoveButton(workOrderItem);
+          } else {
+            this.insertSaveButton(workOrderItem);
+          }
+        });
+      });
+    },
+
+    queryElement: function (selector) {
+      if (typeof selector !== "string") throw new Error("Invalid selector.");
+
+      let element = document.querySelector(selector);
+      if (element) return Promise.resolve(element);
+
+      return new Promise((resolve) => {
+        const observer = new MutationObserver((mutationsList, obs) => {
+          element = document.querySelector(selector);
+
+          if (element) {
+            obs.disconnect();
+            resolve(element);
+          }
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+      });
+    },
+
+    insertSaveButton: function (listItem) {
+      if (!(listItem instanceof Element)) throw new Error("Invalid element.");
+
+      const buttonClassName = this.save_button_class;
+      if (listItem.querySelector("." + buttonClassName)) return;
+
+      const saveButton = document.createElement("button");
+      saveButton.className = buttonClassName + " nss-button";
+      saveButton.title = "Grab";
+
+      const iconSave = document.createElement("i");
+      iconSave.className = "icon";
+
+      const svgSave = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+          <path d="M160 96C124.7 96 96 124.7 96 160L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 160C544 124.7 515.3 96 480 96L160 96zM296 408L296 344L232 344C218.7 344 208 333.3 208 320C208 306.7 218.7 296 232 296L296 296L296 232C296 218.7 306.7 208 320 208C333.3 208 344 218.7 344 232L344 296L408 296C421.3 296 432 306.7 432 320C432 333.3 421.3 344 408 344L344 344L344 408C344 421.3 333.3 432 320 432C306.7 432 296 421.3 296 408z"/>
+        </svg>
+      `;
+      iconSave.innerHTML = svgSave;
+
+      const spanSave = document.createElement("span");
+      spanSave.className = "sr-only";
+      spanSave.textContent = "Save Work Order";
+
+      saveButton.addEventListener("click", () => {
+        this.saveWorkOrder(listItem);
+      });
+
+      saveButton.appendChild(iconSave);
+      saveButton.appendChild(spanSave);
+      listItem.appendChild(saveButton);
+    },
+
+    insertRemoveButton: function (listItem) {
+      if (!(listItem instanceof Element)) throw new Error("Invalid element.");
+
+      const buttonClassName = this.remove_button_class;
+      if (listItem.querySelector("." + buttonClassName)) return;
+
+      const removeButton = document.createElement("button");
+      removeButton.className = buttonClassName + " nss-button";
+      removeButton.title = "Remove";
+
+      const iconRemove = document.createElement("i");
+      iconRemove.className = "icon";
+
+      const svgRemove = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+          <path d="M160 96C124.7 96 96 124.7 96 160L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 160C544 124.7 515.3 96 480 96L160 96zM232 296L408 296C421.3 296 432 306.7 432 320C432 333.3 421.3 344 408 344L232 344C218.7 344 208 333.3 208 320C208 306.7 218.7 296 232 296z"/>
+        </svg>
+      `;
+      iconRemove.innerHTML = svgRemove;
+
+      const spanRemove = document.createElement("span");
+      spanRemove.className = "sr-only";
+      spanRemove.textContent = "Remove Work Order";
+
+      removeButton.addEventListener("click", () => {
+        this.removeWorkOrder(listItem);
+      });
+
+      removeButton.appendChild(iconRemove);
+      removeButton.appendChild(spanRemove);
+      listItem.appendChild(removeButton);
+    },
+
+    saveWorkOrder: async function (listItem) {
+      if (!(listItem instanceof Element)) throw new Error("Invalid list item.");
+
+      const workOrderNumber =
+        listItem.querySelector(".js-work-order-number [aria-label='Number']")
+          .textContent || null;
+      if (!workOrderNumber) return;
+
+      const expandButton = listItem.querySelector(
+        "button.js-expand.btn.btn-link:has(i.fa-chevron-down,i.fa-chevron-up)"
+      );
+      if (!expandButton) {
+        console.error("Can't fetch work order data.", workOrderNumber);
+        return;
+      }
+
+      expandButton.click();
+
+      const street =
+        listItem.querySelector("[aria-label='Address']").textContent || "";
+      const cityStateZip =
+        listItem.querySelector("[aria-label='AddressCityAndState']")
+          .textContent || "";
+
+      const description =
+        listItem.querySelector("[aria-label='Description']").textContent || "";
+
+      const regex = /(.+?), ([A-Z]{2}) (\d{5}(?:-\d{4})?)/;
+      const match = cityStateZip.match(regex);
+
+      let city = "";
+      let state = "";
+      let zip = "";
+
+      if (match) {
+        city = (match[1] || "").trim();
+        state = match[2] || "";
+        zip = match[3] || "";
+      }
+
+      expandButton.click();
+
+      const work_order = {
+        number: workOrderNumber || "",
+        street: street || "",
+        city: city || "",
+        state: state || "",
+        zip: zip || "",
+        description: description || "",
+      };
+
+      chrome.storage.local.get("work_orders", (result) => {
+        const data = result.work_orders || {};
+        if (typeof data !== "object") throw new Error("Invalid map data.");
+
+        this.work_orders = new Map(Object.entries(data));
+
+        this.work_orders.set(work_order.number, work_order);
+
+        chrome.storage.local.set(
+          { work_orders: Object.fromEntries(this.work_orders) },
+          () => {}
+        );
+      });
+    },
+
+    removeWorkOrder: async function (listItem) {
+      if (!(listItem instanceof Element)) throw new Error("Invalid list item.");
+
+      const workOrderNumber =
+        listItem.querySelector(".js-work-order-number [aria-label='Number']")
+          .textContent || null;
+      if (!workOrderNumber) return;
+
+      chrome.storage.local.get("work_orders", (result) => {
+        const data = result.work_orders || {};
+        if (typeof data !== "object") throw new Error("Invalid map data.");
+
+        this.work_orders = new Map(Object.entries(data));
+
+        if (!this.work_orders.has(workOrderNumber)) return;
+
+        this.work_orders.delete(workOrderNumber);
+
+        chrome.storage.local.set(
+          { work_orders: Object.fromEntries(this.work_orders) },
+          () => {}
+        );
+      });
+    },
+
+    observeUrlChange: function () {
+      let lastUrl = location.href;
+
+      new MutationObserver(() => {
+        const currentUrl = location.href;
+
+        if (
+          currentUrl !== lastUrl &&
+          location.pathname.includes(
+            "/maintenance/service_requests/work_orders"
+          )
+        ) {
+          lastUrl = currentUrl;
+          content.init();
+        }
+      }).observe(document.body, { childList: true, subtree: true });
+    },
+  };
+
+  content.init();
+  content.observeUrlChange();
+
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName !== "local" && !changes.work_orders) return;
+
+    const allButtons = document.querySelectorAll(".nss-button");
+    if (!allButtons) return;
+
+    allButtons.forEach((button) => button.remove());
+    content.init();
+  });
 })();
